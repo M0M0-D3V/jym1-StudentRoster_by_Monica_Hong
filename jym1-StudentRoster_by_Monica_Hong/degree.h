6d56d3b2318f253818d34470e672a9c98df05cbf
momoxstarx <momo.star@gmail.com>
2024-01-12 04:47:17 -0800

    update roster constructor

--- jym1-StudentRoster_by_Monica_Hong/degree.h
+++ jym1-StudentRoster_by_Monica_Hong/degree.h
@@ -3,7 +3,8 @@ enum class DegreeProgram
 {
 	SECURITY = 0,
 	NETWORK = 1,
-	SOFTWARE = 2
+	SOFTWARE = 2,
+	NONE = 3
 };
 
 //[x] C.  Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
--- jym1-StudentRoster_by_Monica_Hong/roster.cpp
+++ jym1-StudentRoster_by_Monica_Hong/roster.cpp
@@ -1,27 +1,78 @@
 #include <iostream>
+#include <sstream>
+//#include <vector>
 using namespace std;
 
 #include "roster.h"
 
-Roster::Roster()
+Roster::Roster(const string students[])
 {
+	studentCount = students->length();
+	ClassRosterArray = new Student*[studentCount];
 
+	//E2a. Parse tokens (string)
+	for (int i; i < studentCount; i)
+	{
+		// Use vector of string to save info bits for each student
+		vector<string> info;
+		// stringstream consolidates the string object with a string
+		// this allows string to read similar to "cin"
+		stringstream streamInput(students[i]);
+		string nextInfo;
+
+		// Separating details by ','
+		while (getline(streamInput, nextInfo, ','))
+		{
+			info.push_back(nextInfo);
+		}
+
+		// string to int conversion
+		int age = stoi(info[4]);
+		int daysToComplete1 = stoi(info[5]);
+		int daysToComplete2 = stoi(info[6]);
+		int daysToComplete3 = stoi(info[7]);
+
+		// string to enum conversion
+		DegreeProgram degree;
+		string degreeInput = info[8];
+
+		if (degreeInput.compare("SECURITY") == 0)
+		{
+			degree = DegreeProgram::SECURITY;
+		}
+		else if (degreeInput.compare("NETWORK") == 0)
+		{
+			degree = DegreeProgram::NETWORK;
+		}
+		else if (degreeInput.compare("SOFTWARE") == 0)
+		{
+			degree = DegreeProgram::SOFTWARE;
+		}
+		else
+		{
+			degree = DegreeProgram::NONE;
+		}
+
+		// Adding each student object to ClassRosterArray
+		ClassRosterArray[i] = new Student(info[0], info[1], info[2], info[3], age, daysToComplete1, daysToComplete2, daysToComplete3, degree);
+	}
 }
 
 Roster::~Roster()
 {
-
+	delete[] ClassRosterArray;
 }
 
 void Roster::Add(string id, string fName, string lName, string email, int age, int day1, int day2, int day3, DegreeProgram degree)
 {
-	Student newStudent(id, fName, lName, email, age, day1, day2, day3, degree);
-	ClassRosterArray[studentCount] = newStudent;
+	Student* student = new Student(id, fName, lName, email, age, day1, day2, day3, degree);
+	ClassRosterArray[studentCount] = student;
 	studentCount++;
 }
 
 void Roster::Remove(string id)
 {
+
 
 }
 
@@ -46,17 +97,19 @@ void Roster::PrintByDegreeProgram(DegreeProgram degree) const
 }
 
 //[] E.  Create a Roster class (roster.cpp) by doing the following:
-//    [] 1.  Create an array of pointers, classRosterArray, to hold the data provided in the “studentData Table.”
+//    [x] 1.  Create an array of pointers, classRosterArray, to hold the data provided in the “studentData Table.”
 //    
-//    [] 2.  Create a student object for each student in the data table and populate classRosterArray.
-//        [] a.  Parse each set of data identified in the “studentData Table.”
-//        [] b.  Add each student object to classRosterArray.
+//    [x] 2.  Create a student object for each student in the data table and populate classRosterArray.
+//        [x] a.  Parse each set of data identified in the “studentData Table.”
+//        [x] b.  Add each student object to classRosterArray.
 //    
 //    [] 3.  Define the following functions
-//        [] a.  public void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram)  that sets the instance                   variables from part D1 and updates the roster.
+//        [x] a.  public void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram)  that sets the instance                   variables from part D1 and updates the roster.
 //        [] b.  public void remove(string studentID)  that removes students from the roster by student ID. If the student ID does not exist, the function prints an error message indicating that the student was not found.
 //        [] c. public void printAll() that prints a complete tab-separated list of student data in the provided format: A1 [tab] First Name: John [tab] Last Name: Smith [tab] Age: 20 [tab]daysInCourse: {35, 40, 55} Degree                      Program: Security. The printAll() function should loop through all the students in classRosterArray and call the print() function for each student.
 //        [] d.  public void printAverageDaysInCourse(string studentID)  that correctly prints a student’s average number of days in the three courses. The student is identified by the studentID parameter.
 //        [] e.  public void printInvalidEmails() that verifies student email addresses and displays all invalid email addresses to the user.
 //        
 //        Note: A valid email should include an at sign ('@') and period ('.') and should not include a space (' ').
+//        
+//        [] f.  public void printByDegreeProgram(DegreeProgram degreeProgram) that prints out student information for a degree program specified by an enumerated type.
